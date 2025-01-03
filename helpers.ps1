7866a4f5727989f24f8897d8bbb1a393e748bc93
Prince <65974917+psycho-prince@users.noreply.github.com>
2025-01-03 11:00:25 +0530

    Create helpers.ps1

---
+++ helpers.ps1
@@ -0,0 +1,6 @@
+function Check-Admin {
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Host "This script must be run as Administrator" -ForegroundColor Red
+        exit
+    }
+}
